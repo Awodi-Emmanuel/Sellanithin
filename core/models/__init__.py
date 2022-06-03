81a91fe3ade429686cc97e7f51ebce34d851b532
NigerianCoder <gbemilanre@gmail.com>
2022-06-03 22:49:09 +0100

    completed product serializer

---
+++ core/models/__init__.py
@@ -0,0 +1,7 @@
+from .abstration import Product
+from .abstration import Category
+
+__all__ = (
+    'Product',
+    'Category'
+)
---
+++ core/models/model_serializer.py
@@ -0,0 +1,36 @@
+from dataclasses import field
+from rest_framework.serializers import CharField, IntegerField, ListSerializer
+from rest_framework.serializers import ModelSerializer
+
+from core.models.abstration import Category, Product 
+
+class CategorySerializer(ModelSerializer):
+    id = IntegerField()
+    
+    class Meta:
+        model = Category
+        fields = (
+            "name",
+            "slug"
+        )   
+
+class ProductSerializer(ModelSerializer):
+    id = IntegerField()
+    
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "category",
+            "name",
+            "slug",
+            "description",
+            "price",
+            "image",
+            "thumbnail",
+            "stock",
+            "available",
+            "date_added",
+            "updated"
+        )
+        
