b0f4db200304c940d14e49cbe0ae30305899a4b1
NigerianCoder <gbemilanre@gmail.com>
2022-06-12 01:34:26 +0100

    password reset implementation completed

--- core/api_views.py
+++ core/api_views.py
@@ -607,7 +607,41 @@ class AuthViewset(YkGenericViewSet, UpdateModelMixin):
         try:
             rcv_ser = ResetWithPasswordSerializer(data=self.request.data)
             if rcv_ser.is_valid():
-                print(rcv_ser)
+                code = base.url_safe_decode(rcv_ser.validated_data["code"])
+                email = base.url_safe_decode(rcv_ser.validated_data["email"])
+                tmp_code = (
+                    TempCode.objects.filter(
+                        code=code,
+                        user__email=email,
+                        is_used=False,
+                        expires__gte=timezone.now()
+                    )
+                    .select_related()
+                    .first()
+                )
+                if tmp_code:
+                    tmp_code.user.is_active=True
+                    tmp_code.user.set_password(rcv_ser.validated_data["email"])
+                    tmp_code.user.save()
+                    tmp_code.is_used=True
+                    tmp_code.save()
+                    
+                    tpl = "reset"
+                    message = {
+                        "subject": _("Password reset confirmation"),
+                        "tpl": tpl
+                    }
+                    
+                    # TODO Create Apache Kafka Notification
+                    
+                    user_ser = UserSerializer(tmp_code.user)
+                    return GoodResponse(user_ser.data)
+                else:
+                    return NotFoundResponse(
+                        "Token expired or invalid",
+                        "invalid_data",
+                        request=self.request,
+                    )
             else:
                 return BadRequestResponse(
                     "Unable to reset",
